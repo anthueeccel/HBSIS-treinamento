9d14314d12bbb18611e65d1b2e6fa2cf482322bd
Anthue <anthue@gmail.com>
2019-09-10 17:31:22 -0300

    Commit ConsoleApp1

---
+++ Aula2/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Criado por: Anthue
+/// Data: 2019-09-10
+/// </summary>
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Texto informativo para consultar o livro pelo número do registro do sistema
+            Console.WriteLine("Infrome o livro a ser consultado: ");
+            //Parte do codigo que recebe as informações do registro do livro e coloca na variável numeroDoLivro para utilizar
+            var numeroDoLivro = Console.ReadLine();
+
+
+            if (numeroDoLivro == "123456")
+            {
+                Console.WriteLine("Livro inddisponível!!!!");
+                Console.ReadKey();
+                //Finaliza o método
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Deseja locar o livro ? (1) Sim (2) Não ");
+                var resposta = Console.ReadLine();
+                if (resposta == "1")
+                {
+                    Console.WriteLine("Livro locado.");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+        }
+    }
+}

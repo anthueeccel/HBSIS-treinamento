c5b9c61050083bbc31d26db1d267266770078f31
Anthue <anthue@gmail.com>
2019-09-18 16:25:49 -0300

    Aula 8 - utlizando outra DLL

---
+++ Aula7/Crazy/AcessandoDllDoAmiguinho/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinhaBiblicoteca;
+
+namespace AcessandoDllDoAmiguinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaBiblicoteca.MyFuckingTree.StupidTree();
+            MinhaBiblicoteca.ListaDeMarcas.ClasseListaCarros();
+            MinhaBiblicoteca.ListaDeMarcas.ClasseListaCervejas();
+
+            var calculo = new MinhaBiblicoteca.CalculoDeArea();
+            Console.Write("Lado do quadrado: ");
+            int lado = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Área do quadrado: {calculo.CalculaAreaQuad(lado)}");
+            Console.ReadKey();
+        }
+    }
+}

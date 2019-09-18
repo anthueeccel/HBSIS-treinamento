d1c1da434ccf2ba1e39f13f5963eb64ac00216ae
Anthue <anthue@gmail.com>
2019-09-18 17:25:56 -0300

    Aula 8 - primeira Classe

---
+++ Aula8/MinhaPrimeiraClass/MinhaSegundaClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using MinhaSegundaClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestaDeFrutas = new List<Frutas>
+            {
+                new Frutas()
+                {
+                    Nome = "Banana",
+                    Quantidade = 3
+                },
+                new Frutas()
+                {
+                    Nome = "Morango",
+                    Quantidade = 5
+                },
+                new Frutas()
+                {
+                    Nome = "Tomate",
+                    Quantidade = 2
+                }
+
+             };
+            minhaCestaDeFrutas.ForEach(i =>
+            Console.WriteLine($"Nome: {i.Nome}, quantidade: {i.Quantidade}"));
+            Console.ReadKey();
+        }
+    }
+}

0b765c1d0a09d98cc68935934422d29f3f1873e7
Anthue <anthue@gmail.com>
2019-09-24 11:29:47 -0300

    Aula 12 - refatorando Listas/LINQ

--- OrdenandoEFiltrandoListas/FiltrosLinq/Controller/PessoaController.cs
+++ OrdenandoEFiltrandoListas/FiltrosLinq/Controller/PessoaController.cs
@@ -10,58 +10,107 @@ namespace FiltrosLinq.Controller
 {
     class PessoaController
     {
-        public List<Pessoa> listaPessoas = new List<Pessoa>();
-        public Pessoa pessoa = new Pessoa();
 
-     /// <summary>
-     /// Carrega a lista de pessoas de um arquivo txt separados por ";"
-     /// </summary>
-     /// <returns>listaDePessoas</returns>
-        public List<Pessoa> CarregaLista()
+        private List<Pessoa> ListaDePessoas { get; set; }
+        //public List<Pessoa> listaPessoas = new List<Pessoa>();
+
+        // Consturtor da classe
+        public PessoaController()
         {
-            var listaDePessoas = new List<Pessoa>();
+            ListaDePessoas = CarregaLista();
+        }
+
+
+        /// <summary>
+        /// Carrega a lista de pessoas de um arquivo txt separados por ";"
+        /// </summary>
+        /// <returns>listaDePessoas</returns>
+        private List<Pessoa> CarregaLista()
+        {
             //Instância da lista que será preenchida
+            //var listaDePessoas = new List<Pessoa>();
+            ListaDePessoas = new List<Pessoa>();
 
             //Retorna todas as linhas do arquivo em um array
             //de string, onde cada linha será um índice do array
             string[] array = File.ReadAllLines(@"C:\temp\listaPessoas.txt");
 
-            //percorro o array e para cada linha
+            //percorre o array e para cada linha
             for (int i = 0; i < array.Length; i++)
             {
-                //crio um objeto do tipo Pessoa
+                //cria um objeto do tipo Pessoa
                 Pessoa c = new Pessoa();
 
-                //Uso o método Split e quebro cada linha
-                //em um novo array auxiliar, ou seja, cada
-                //conteúdo do arquivo txt separado por ';' será
-                //um nova linha neste array auxiliar. Assim sei que
-                //cada índice representa uma propriedade
+                //Split para 'quebrar' as linhas da Array em um Array auxiliar 
+                //do arquivo txt carregado, separando por ';'. Cada índice 
+                // representa uma propriedade da Classe
                 string[] auxiliar = array[i].Split(';');
 
-                //Aqui recupero os itens, atribuindo
-                //os mesmo as propriedade da classe
-                //Pessoa correspondentes, ou seja,
-                //o índice zero será corresponde ao Id                
+                // Preencher os dados da Array nas propriedades da Classe
+                // na List<Pessoa>
                 c.Id = Convert.ToInt32(auxiliar[0]);
                 c.Nome = auxiliar[1];
                 c.DateNascimento = DateTime.Parse(auxiliar[2]);
                 c.Carteira = double.Parse(auxiliar[3]);
-                
-                //Adiciono o objeto a lista
-                listaDePessoas.Add(c);
+
+                ListaDePessoas.Add(c);
 
             }
-            return listaDePessoas;
+            return ListaDePessoas;
+        }
+
+
+        // Cria uma lista List<Pessoa> pública que pdoe ser apenas acessada 'get', 
+        // assim não permite-se que a lista seja alterada
+        public List<Pessoa> ListaDePessoasPublic
+        {
+            get { return ListaDePessoas; }
         }
 
         /// <summary>
-        /// Método que retornar uma lista com pessoas maiores de 18 anos
+        /// Método que retorna lista de pessoas ordenada pelo nome
+        /// de A-Z (Ascendente).
         /// </summary>
-        /// <returns>listaPessoas</returns>
-        public List<Pessoa> RetornaListaPorIdadeMaior18()
+        /// <returns>Lista de Pessoas</returns>
+        public List<Pessoa> GetPessoasOrdenadaAscendente()
         {
-            return listaPessoas.FindAll(x => (DateTime.Now.Year - x.DateNascimento.Year) >= 18);
+            return ListaDePessoas
+                .OrderBy(x => x.Nome)
+                .ToList<Pessoa>();
+        }
+
+        /// <summary>
+        /// Método que retorna lista de pessoas ordenadas descedente por 
+        /// data de nascimento.
+        /// </summary>
+        /// <returns>lista de Pessoa</returns>
+        public List<Pessoa> GetPessoasOrdenadasDescDtNasc()
+        {
+            return ListaDePessoas
+                .OrderByDescending(x => x.DateNascimento)
+                .ToList<Pessoa>();
+        }
+        /// <summary>
+        /// Método que retorna lista de pessoas com mais de 500 na carteira
+        /// </summary>
+        /// <param name="valor">Valor informado para o filtro.
+        /// Caso não informe valor, o padrão será 500</param>
+        /// <returns>Lista de pessoas</returns>
+        public List<Pessoa> GetPessoasCarteiraMaior500(double valor = 500)
+        {
+            return ListaDePessoas
+                .FindAll(c => c.Carteira > valor)
+                .OrderBy(c => c.Carteira).ToList<Pessoa>();
+        }
+
+        /// <summary>
+        /// Método que retorna uma lista com pessoas maiores de 18 anos ou valor informado
+        /// </summary>
+        /// <returns>listaPessoas com idade maior que a informada, ou por padrão >18</returns>
+        public List<Pessoa> RetornaListaPorIdadeMaior18(int idade = 18)
+        {
+            return ListaDePessoas
+                .FindAll(x => (DateTime.Now.Year - x.DateNascimento.Year) >= idade);
         }
 
         /// <summary>
@@ -70,9 +119,21 @@ namespace FiltrosLinq.Controller
         /// <returns>listaPessoas</returns>
         public List<Pessoa> RetornaListaPorIdadeMenor16()
         {
-            return listaPessoas.FindAll(x => (DateTime.Now.Year - x.DateNascimento.Year) <= 16);
+            return ListaDePessoas.FindAll(x => (DateTime.Now.Year - x.DateNascimento.Year) <= 16);
         }
 
-
+        public List<Pessoa> getPessoasPorIdadeMaiorOuMenor(int idade = 17)
+        {
+            if (idade >= 18)
+            {
+                return ListaDePessoas
+                    .FindAll(x => (DateTime.Now.Year - x.DateNascimento.Year) >= idade);
+            }
+            else
+            {
+                return ListaDePessoas
+                    .FindAll(x => (DateTime.Now.Year - x.DateNascimento.Year) <= idade);
+            }
+        }
     }
 }
--- OrdenandoEFiltrandoListas/FiltrosLinq/Program.cs
+++ OrdenandoEFiltrandoListas/FiltrosLinq/Program.cs
@@ -13,43 +13,74 @@ namespace FiltrosLinq
         {
             PessoaController pessoaController = new PessoaController();
 
-            var listaDePessoas = pessoaController.CarregaLista();
+            var listaDePessoas = pessoaController.ListaDePessoasPublic;
 
-            Console.WriteLine("\n-----------------Lista Original");
+            ImprimeTag("Lista Original");
             listaDePessoas
                 .OrderBy(o => o.Id).ToList<Pessoa>()
-                .ForEach(c => Console.WriteLine($"Id: {c.Id}| Nome: {c.Nome}| Data Nascimento: {c.DateNascimento.ToShortDateString()}| Carteira: R${c.Carteira}"));
+                .ForEach(c => ImprimeDados(c));
 
-            Console.WriteLine("\n-----------------Lista Ordem por Nome");
-            listaDePessoas.OrderBy(h => h.Nome).ToList<Pessoa>()
-                .ForEach(c => Console.WriteLine($"Id: {c.Id}| Nome: {c.Nome}| Data Nascimento: {c.DateNascimento.ToShortDateString()}| Carteira: R${c.Carteira}"));
+            ImprimeTag("Lista Ordem por Nome");
+            pessoaController.GetPessoasOrdenadaAscendente()
+                .ForEach(c => ImprimeDados(c));
 
-            Console.WriteLine("\n-----------------Lista Ordem por Data de Nascimento");
-            listaDePessoas.OrderByDescending(h => h.DateNascimento).ToList<Pessoa>()
-                .ForEach(c => Console.WriteLine($"Id: {c.Id}| Nome: {c.Nome}| Data Nascimento: {c.DateNascimento.ToShortDateString()}| Carteira: R${c.Carteira}"));
+            ImprimeTag("Lista Ordem por Data de Nascimento");
+            pessoaController.GetPessoasOrdenadasDescDtNasc()
+                .ForEach(c => ImprimeDados(c));
 
-            Console.WriteLine("\n-----------------Lista Pessoas > R$500");
-            (from dinheiroCarteira in listaDePessoas
-             where dinheiroCarteira.Carteira > 500
-             select dinheiroCarteira).ToList<Pessoa>()
-                .ForEach(c => Console.WriteLine($"Id: {c.Id}| Nome: {c.Nome}| Data Nascimento: {c.DateNascimento.ToShortDateString()}| Carteira: R${c.Carteira}"));
+            ImprimeTag($"Lista Pessoas > R${500}");
+            //(from dinheiroCarteira in listaDePessoas
+            // where dinheiroCarteira.Carteira > 500
+            // select dinheiroCarteira).ToList<Pessoa>()
 
-            Console.WriteLine("\n-----------------Lista Pessoas > 18 anos");
-            pessoaController.RetornaListaPorIdadeMaior18()
-                .ForEach(c => Console.WriteLine($"Id: {c.Id}| Nome: {c.Nome}| Data Nascimento: {c.DateNascimento.ToShortDateString()}| Carteira: R${c.Carteira}"));
+            //Console.WriteLine("Valor: ");
+            //double valor = 0;
+            //double.TryParse(Console.ReadLine(), out valor);
+
+            pessoaController.GetPessoasCarteiraMaior500()
+             .ForEach(c => ImprimeDados(c));
 
 
-            Console.WriteLine("\n-----------------Lista Pessoas < 16 anos");
+            ImprimeTag($"Lista Pessoas > {18} anos");
+            pessoaController.RetornaListaPorIdadeMaior18(18)
+                .ForEach(c => ImprimeDados(c));
+
+            ImprimeTag($"Lista Pessoas < {16} anos");
             pessoaController.RetornaListaPorIdadeMenor16()
-                             .ForEach(c => Console.WriteLine($"Id: {c.Id}| Nome: {c.Nome}| Data Nascimento: {c.DateNascimento.ToShortDateString()}| Carteira: R${c.Carteira}"));
+                .ForEach(c => ImprimeDados(c));
 
-
-
-
-
-
+            Console.WriteLine("Filtro para idade maior ou menor");
+            Console.WriteLine("Filtro para idade: ");
+            int idade;
+            int.TryParse(Console.ReadLine(), out idade);
+            pessoaController.getPessoasPorIdadeMaiorOuMenor(idade)
+                .ForEach(c => ImprimeDados(c));
 
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Método para imprimir no console os dados da Classe Pessoa.
+        /// </summary>
+        /// <param name="pessoa"></param>
+        public static void ImprimeDados(Pessoa pessoa)
+        {
+            string template = "Id: {0,-3} | Nome: {1,-10} | Data Nascimento: {2,10} | Carteira: {3,8}";
+            string textoFormatado = string.Format(template,
+                pessoa.Id,
+                pessoa.Nome,
+                pessoa.DateNascimento.ToShortDateString(),
+                pessoa.Carteira.ToString("C2"));
+            Console.WriteLine(textoFormatado);
+        }
+
+        /// <summary>
+        /// Método que imprime o nome da ação executada (listagem)
+        /// </summary>
+        /// <param name="nomeMetodo">Identificador da Listagem</param>
+        public static void ImprimeTag(string nomeMetodo)
+        {
+            Console.WriteLine(string.Format("\n======================== {0,0} =======================", nomeMetodo));
         }
     }
 }

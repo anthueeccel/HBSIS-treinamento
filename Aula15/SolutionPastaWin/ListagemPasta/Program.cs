3ed82cfd1606030c1d5443a1084b2084dadee0e6
Anthue <anthue@gmail.com>
2019-09-27 17:24:04 -0300

    PastaWin - Manipulação de pastas em MeusDocumentos

--- Aula15/SolutionPastaWin/ListagemPasta/Program.cs
+++ Aula15/SolutionPastaWin/ListagemPasta/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DllPastaWin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,103 @@ namespace ListagemPasta
 {
     class Program
     {
+
+        static DocumentsAutoGenerate doc = new DocumentsAutoGenerate();
+        //Directory
+
         static void Main(string[] args)
         {
+
+            var menu = int.MinValue;
+
+            while (menu != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de Pasta");
+                Console.WriteLine("1 - Listar as Pastas em MeusDocumentos");
+                Console.WriteLine("2 - Criar Pasta em MeusDocumentos");
+                Console.WriteLine("3 - Excluir Pasta em MeusDocumentos");
+                Console.WriteLine("0 - Sair");
+
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        ListarPasta();
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        CriarPasta();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        ExcluirPasta();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        new HoraDoShow().Birllllllllll();
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+
         }
+
+        public static void ListarPasta()
+        {
+            try
+            {
+                var listaPasta = doc.ObterPastasDiretorioMeuDocumentos().ToList();
+                listaPasta.ForEach(x => Console.WriteLine($"{x.ToString()}"));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro ao criar pasta");
+                throw;
+            }
+
+        }
+
+        public static void CriarPasta()
+        {
+            Console.Write("Criar Pasta - nome: ");
+            string nomeDaPasta = Console.ReadLine();
+            Console.WriteLine("Criar pasta? " + nomeDaPasta);
+            var criarPasta = Console.ReadLine().ToLower();
+            try
+            {
+                if (criarPasta == "s")
+                    doc.CriarPastaMeusDocumentos(nomeDaPasta);
+                Console.WriteLine("Pasta criada com sucesso!");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro ao Criar a pasta");
+                throw;
+            }
+        }
+
+        public static void ExcluirPasta()
+        {
+            Console.Write("Qual pasta quer excluir? ");
+            var nomeDaPasta = Console.ReadLine();
+            try
+            {
+                doc.DeletarPastaMeusDocumentos(nomeDaPasta, true);
+                Console.WriteLine("Excluída com sucesso!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro ao excluir");
+                throw;
+            }
+        }
+
     }
 }

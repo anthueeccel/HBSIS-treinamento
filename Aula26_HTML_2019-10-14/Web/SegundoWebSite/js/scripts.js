0522e7239d85634d662ceae3fa754a019018f521
Anthue Eccel <anthue@gmail.com>
2019-10-17 17:30:36 -0300

    Exercicios WEB - convert moedas e boas vindas com modal

--- Aula26_HTML_2019-10-14/Web/SegundoWebSite/js/scripts.js
+++ Aula26_HTML_2019-10-14/Web/SegundoWebSite/js/scripts.js
@@ -1,11 +1,25 @@
-function enviar(){
-    var nome = document.getElementById("nome").value;
-    // alert("Olá " + nome);
-}
+// carrega as funções após carregar a página.
+$(document).ready(function () {
+    $('button[name=btnboasvindas]').click(function () {
+        var nomeusuario = $('input[name="nome"]').val();
+        $('span[name="usuariotext"]').text(nomeusuario);
+    });
+});
 
 
+//Evento ON não é carregado na leitura, somente no evento, quando o evento é chamado.
+// $('button[name=btnboasvindas]').on("click", function () {
+//     $('button[name=btnboasvindas]').click(function () {
+//         var nomeusuario = $('input[name="nome"]').val();
+//         $('span[name="usuariotext"]').text(nomeusuario);
+//     });
+// });
 
-$('#myModal').on('show.bs.modal', function(e) {
-    var bookId = $(e.relatedTarget).data('nome');
-    $(e.currentTarget).find('input[name="nome"]').val(bookId);
-})
+
+//habilita o ENTER para executar o evento.
+$(document).on('keypress', function (e) {
+    if (e.which == 13) {        
+        $('button[name="btnboasvindas"]').click();
+    }
+});
+
--- Aula26_HTML_2019-10-14/Web/WebCurrencyConverter/js/scripts.js
+++ Aula26_HTML_2019-10-14/Web/WebCurrencyConverter/js/scripts.js
@@ -1,42 +1,23 @@
 $(document).ready(
-    function () {
-        //$('input[name="valor1"]').mask("000.000.000,00");
-
-        // var getJSON = function(url, callback) {
-        //     var xhr = new XMLHttpRequest();
-        //     xhr.open('GET', url, true);
-        //     xhr.responseType = 'json';
-        //     xhr.onload = function() {
-        //       var status = xhr.status;
-        //       if (status === 200) {
-        //         callback(null, xhr.response);
-        //       } else {
-        //         callback(status, xhr.response);
-        //       }
-        //     };
-        //     xhr.send();
-        // };
-
-        $.getJSON("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2710-16-2019%27&$top=100&$format=json",
-           function(data) {
-               if (err !== null) {
-                 alert('Erro: ' + err);
-               } else {
-                 alert('JSON: ' + valor);
-               }
-             });
-             console.log(data);
+  function () {
+    $('input[name="valor1"]').mask("000.000,00");
 
 
-        $('input[name=valor2]').val(data);
-
-        $('button[id="calcular"]').click(function () {
-            var valor1 = $('input[name="valor1"]').val();
-            var valor2 = $('input[name="valor2"]').val();
-            var result =  parseFloat(valor1) * parseFloat(valor2);
-            //alert(result);
-            $('input[name="result"]').val(result);
-            
-
-        })
+    $.getJSON({
+      url: "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2710-17-2019%27&$top=100&$format=json",
+      success: function (result, status, xhr) {
+        //debugger                          
+        $('input[name=valor2]').val(parseFloat(result.value[0].cotacaoVenda).toFixed(2));
+      }
     });
+
+    $('button[id="calcular"]').click(function () {
+      var valor1 = $('input[name="valor1"]').val()
+      .replace(".", "")
+      .replace(",", "")
+      .toFixed(2);
+      var valor2 = $('input[name="valor2"]').val();
+      var result = parseFloat(valor1) / parseFloat(valor2);
+      $('input[name="result"]').val(result.toFixed(2));
+    })
+  });

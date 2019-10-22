c5c7b82daff3088dc69452349717b8d5e7bca68f
Anthue Eccel <anthue@gmail.com>
2019-10-22 14:31:04 -0300

    WebApi ViaCEP

---
+++ Aula32_WebApp_2019-10-22/WebApplication1/WebApplication1/Utils/scripts.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $('#button1').click(function () {
+        $.getJSON("https://viacep.com.br/ws/SC/Blumenau/all/json/", function (data) {
+            console.log(data);            
+            
+            var templateContent = '';
+
+            templateContent += '<div><table class="table"><tbody>' +
+                '<th>Valor</th>' +
+                '<th>Localidade</th>' +
+                '<th>Código CEP</th>' +
+                '<th>Logradouro</th>' +
+                '<th>Bairro</th>' +
+                '<th>IBGE</th>' +
+                '<th>GIA</th>' +
+                '<th>Complemento</th>';
+
+            $.each(data, function (key, value) {
+                templateContent += "<tr><td>" + key + "</td> "
+                    + "<td>" + value.localidade + "</td>"
+                    + "<td style='width:40px'>" + value.cep + "</td>"
+                    + "<td>" + value.logradouro + "</td>"
+                    + "<td>" + value.bairro + "</td>"
+                    + "<td>" + value.ibge + "</td>"
+                    + "<td>" + value.gia + "</td>"
+                    + "<td>" + value.complemento + "</td></tr>";
+            });
+            $('#divContent').append("" + templateContent + "");
+            templateContent += '</td></tbody><table></div>';
+        });
+    });
+});
+
+

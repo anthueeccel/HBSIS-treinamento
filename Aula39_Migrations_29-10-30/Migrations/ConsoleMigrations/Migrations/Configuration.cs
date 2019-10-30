dd92dc8c936ff5b859d0670a43c697b5f8078f9a
Anthue Eccel <anthue@gmail.com>
2019-10-30 13:54:23 -0300

    Migration

---
+++ Aula39_Migrations_29-10-30/Migrations/ConsoleMigrations/Migrations/Configuration.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ConsoleMigrations.Model.MigrationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ConsoleMigrations.Model.MigrationContext";
+        }
+
+        protected override void Seed(ConsoleMigrations.Model.MigrationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            context.Usuarios.AddOrUpdate(x => x.Nome,new Model.Usuario() {
+                Nome = "Anthue",
+                Login = "admin",
+                Senha = "admin",
+                Email = "admin@admin.com.br",
+                Nivel = Model.Nivel.Administrador
+
+            });
+
+            //context.Usuarios.AddOrUpdate(new Model.Usuario()
+            //{
+            //    Nome = "Anthue",
+            //    Login = "admin",
+            //    Senha = "admin",
+            //    Email = "admin@admin.com.br"
+
+            //});
+
+
+        }
+    }
+}
---
+++ Aula39_Migrations_29-10-30/Migrations/ConsoleMigrations/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleMigrations.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMigrations
+{
+    class Program
+    {
+        static MigrationContext context = new MigrationContext();
+        static void Main(string[] args)
+        {
+            context.Usuarios.ToList().ForEach(x => Console.WriteLine($"Nome {x.Nome} Login {x.Login} Nivel {x.Nivel}"));
+
+            Console.ReadKey();
+        }
+    }
+}
